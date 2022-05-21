a7d9be1314bf4224db7ab01a4a005cecf171fa7d
Praveen Dias <praveendias1180@gmail.com>
2022-05-22 00:14:16 +0530

    my-json-server

---
+++ mail/async.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+const link = 'https://my-json-server.typicode.com/praveendias1180/json_server_demo/products'
+
+fetch(link)
+    .then(response =>  response.json())
+    .then(data => console.log(data))
+    .catch(e => console.error(e))
+    .finally(() => console.log('Finished...'))
