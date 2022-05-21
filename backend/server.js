d3e193747d765f22a358beaf29addc63254cd84c
Praveen Dias <praveendias1180@gmail.com>
2022-05-21 18:32:03 +0530

    Implemented GET method

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ var express = require("express");
 var db = require("./database.js");
 var app = express();
 
+var cors = require('cors');
+app.use(cors({
+  origin: "*"
+}))
+
 // Start server
 var HTTP_PORT = 8080;
 
@@ -11,6 +16,25 @@ app.listen(HTTP_PORT, () => {
 
 //HTTP GET method
 app.get("/api/products", (req, res, next) => {
+  try{
+    let sql = 'select * from products';
+    let params = [];
+
+    db.all(sql, params, (err, rows) => {
+      if(err){
+        res.status(400).json({error: err.message});
+        return;
+      }
+
+      res.status(200).json({
+        message: "success",
+        data: rows,
+      })
+    })
+
+  } catch(e){
+    res.status(400).send(e)
+  }
   
 });
 
